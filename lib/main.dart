f032719676a4ffbeebd0214e375c79528c0cec2b
EngFahd <fahdmahmoud520@gmail.com>
2023-10-26 23:38:08 +0200

    add font family to app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:book_store/constanceApp.dart';
 import 'package:book_store/features/spashview/presntation/views/splashviwe.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BookApp());
@@ -14,7 +15,11 @@ class BookApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kMainColor),
+      theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kMainColor,
+          // =============== remmber this
+          textTheme: GoogleFonts.montserratAlternatesTextTheme(
+              ThemeData.dark().textTheme)),
       home: const SpashViwe(),
     );
   }
