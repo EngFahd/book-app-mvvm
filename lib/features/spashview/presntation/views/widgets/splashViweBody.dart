ce7d6b4dcca6f61100c6bfbbe3eaf458c3fb1410
EngFahd <fahdmahmoud520@gmail.com>
2023-10-26 20:25:20 +0300

    adding sliding animations

--- lib/features/spashview/presntation/views/widgets/splashViweBody.dart
+++ lib/features/spashview/presntation/views/widgets/splashViweBody.dart
@@ -3,9 +3,38 @@ import 'dart:math';
 import 'package:book_store/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViweBody extends StatelessWidget {
+class SplashViweBody extends StatefulWidget {
   const SplashViweBody({super.key});
 
+  @override
+  State<SplashViweBody> createState() => _SplashViweBodyState();
+}
+
+class _SplashViweBodyState extends State<SplashViweBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;// give valu 0 to 2
+  late Animation<Offset> slidinganimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidinganimation = Tween<Offset>(
+      begin: const Offset(0, 5),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+   
+    // dont forgite Slideanimation widget on the widget animated and give position
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    animationController.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -17,10 +46,17 @@ class SplashViweBody extends StatelessWidget {
         const SizedBox(
           height: 10,
         ),
-        const Text(
-          "Fahd",
-          textAlign: TextAlign.center,
-        )
+        AnimatedBuilder(
+            animation: slidinganimation,
+            builder: (context, child) {
+              return SlideTransition(
+                position: slidinganimation,
+                child: const Text(
+                  "Read free books",
+                  textAlign: TextAlign.center,
+                ),
+              );
+            })
       ],
     );
   }
