4ddd46331a6bafe40a42979edf5193bc18016f3b
EngFahd <fahdmahmoud520@gmail.com>
2023-10-26 17:52:23 +0300

    create sturecture mvvm

--- lib/features/spashview/presntation/views/splashviwe.dart
+++ lib/features/spashview/presntation/views/splashviwe.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class SpashViwe extends StatelessWidget {
   const SpashViwe({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return const SplashViweBody();
