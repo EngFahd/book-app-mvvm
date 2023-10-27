a9a002725ffd69a8568fb8e146388c66c6fde1d6
EngFahd <fahdmahmoud520@gmail.com>
2023-10-27 19:15:11 +0200

    create book detiles

---
+++ lib/features/home/presentaion/views/book-Detils-viwe.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookDetilesViwe extends StatelessWidget {
+  const BookDetilesViwe({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
