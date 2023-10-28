562639a5c4ba92c2487b902baae1b77c606ea249
EngFahd <fahdmahmoud520@gmail.com>
2023-10-28 19:43:20 +0200

    add titel to detiles

--- lib/features/home/presentaion/views/widgets/book-Detiles-body.dart
+++ lib/features/home/presentaion/views/widgets/book-Detiles-body.dart
@@ -1,4 +1,6 @@
+import 'package:book_store/core/utils/Styles.dart';
 import 'package:book_store/features/home/presentaion/views/widgets/CustemBookDetielsAppBar.dart';
+import 'package:book_store/features/home/presentaion/views/widgets/custem_Listviwe_item.dart';
 import 'package:flutter/material.dart';
 
 class BookDetiledBody extends StatelessWidget {
@@ -6,12 +8,33 @@ class BookDetiledBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
+    var wideth = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
-        children: [CustemBookDetielsAppBar()],
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const CustemBookDetielsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: wideth * 0.1),
+            child: const CustemBookImage(),
+          ),
+          const SizedBox(height: 43),
+          Text(
+            "The Jungle Book",
+            style: Style.textStyle30.copyWith(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 6),
+          Opacity(
+            opacity: 0.7,
+            child: Text("Rudyard Kipling",
+                style: Style.textStyle18.copyWith(
+                    fontStyle: FontStyle.italic, fontWeight: FontWeight.w500)),
+          )
+        ],
       ),
     );
   }
 }
-
--- lib/features/home/presentaion/views/widgets/custem_Listviwe_item.dart
+++ lib/features/home/presentaion/views/widgets/custem_Listviwe_item.dart
@@ -12,18 +12,29 @@ class CustemListViweItem extends StatelessWidget {
       padding: const EdgeInsets.only(left: 10),
       child: SizedBox(
         height: MediaQuery.of(context).size.height * 0.3,
-        child: AspectRatio(
-          aspectRatio: 2.7 / 4,
-          child: Container(
-            // width: 100,
-            // height: MediaQuery.of(context).size.height * 0.3,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(18),
-                color: Colors.red,
-                image: const DecorationImage(
-                    fit: BoxFit.fill, image: AssetImage(AssetsData.kTestim))),
-          ),
-        ),
+        child: CustemBookImage(),
+      ),
+    );
+  }
+}
+
+class CustemBookImage extends StatelessWidget {
+  const CustemBookImage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        // width: 100,
+        // height: MediaQuery.of(context).size.height * 0.3,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(18),
+            color: Colors.red,
+            image: const DecorationImage(
+                fit: BoxFit.fill, image: AssetImage(AssetsData.kTestim))),
       ),
     );
   }
