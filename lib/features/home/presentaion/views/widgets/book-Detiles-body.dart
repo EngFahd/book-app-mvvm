b0cbaac64a473819d0b1691df27be4805603a8b7
EngFahd <fahdmahmoud520@gmail.com>
2023-10-28 22:33:23 +0200

    add custem scroll viwe

--- lib/features/home/presentaion/views/widgets/book-Detiles-body.dart
+++ lib/features/home/presentaion/views/widgets/book-Detiles-body.dart
@@ -2,6 +2,7 @@ import 'package:book_store/core/utils/Styles.dart';
 import 'package:book_store/core/widgets/CustemButton.dart';
 import 'package:book_store/features/home/presentaion/views/widgets/CustemBookDetielsAppBar.dart';
 import 'package:book_store/features/home/presentaion/views/widgets/CustemRateListViweItem.dart';
+import 'package:book_store/features/home/presentaion/views/widgets/FeatureListViwe.dart';
 import 'package:book_store/features/home/presentaion/views/widgets/ListVieweBookDetiels.dart';
 import 'package:book_store/features/home/presentaion/views/widgets/buttonBook.dart';
 import 'package:book_store/features/home/presentaion/views/widgets/custem_Listviwe_item.dart';
@@ -13,47 +14,56 @@ class BookDetiledBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var wideth = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        // crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const CustemBookDetielsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: wideth * 0.25),
-            child: const CustemBookImage(),
-          ),
-          const SizedBox(height: 30),
-          Text(
-            "The Jungle Book",
-            style: Style.textStyle30.copyWith(
-              fontWeight: FontWeight.bold,
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const CustemBookDetielsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: wideth * 0.25),
+                  child: const CustemBookImage(),
+                ),
+                const SizedBox(height: 30),
+                Text(
+                  "The Jungle Book",
+                  style: Style.textStyle30.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 6),
+                Opacity(
+                  opacity: 0.7,
+                  child: Text("Rudyard Kipling",
+                      style: Style.textStyle18.copyWith(
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w500)),
+                ),
+                const SizedBox(height: 14),
+                const CustemRateListViweItem(),
+                const SizedBox(height: 25),
+                const ButtonBook(),
+                const Expanded(child: SizedBox(height: 25)),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "You Can Also like",
+                    style:
+                        Style.textStyle14.copyWith(fontWeight: FontWeight.w600),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const ListVieweBookDetiels(),
+                const SizedBox(height: 10),
+              ],
             ),
           ),
-          const SizedBox(height: 6),
-          Opacity(
-            opacity: 0.7,
-            child: Text("Rudyard Kipling",
-                style: Style.textStyle18.copyWith(
-                    fontStyle: FontStyle.italic, fontWeight: FontWeight.w500)),
-          ),
-          const SizedBox(height: 14),
-          const CustemRateListViweItem(),
-          const SizedBox(height: 25),
-          const ButtonBook(),
-          const SizedBox(height: 25),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "You Can Also like",
-              style: Style.textStyle14.copyWith(fontWeight: FontWeight.w600),
-            ),
-          ),
-          const SizedBox(height: 10),
-          const ListVieweBookDetiels()
-        ],
-      ),
+        )
+      ],
     );
   }
 }
-
